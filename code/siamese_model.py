e0beae495508829dea805e47ff4e94110ae8b99f
ThomasChen94 <ThomasChen@Thomas-Work-Station.local>
2018-07-03 16:21:26 +0800

    fix some minor bugs (cannot run yet)

--- code/siamese_model.py
+++ code/siamese_model.py
@@ -2,12 +2,12 @@ from preprocess import *
 from siamese import *
 
 import pickle
-import numpy
+import numpy as np
 import tensorflow as tf
 
 class SiameseModel:
 
-	def __init__():
+	def __init__(self):
 		# add some member
 		self.words = []
 		self.embeddings = []
@@ -16,7 +16,7 @@ class SiameseModel:
 		self.num_epochs = 5
 		self.test_gap = 100
 
-	def load_previous_model(sess):
+	def load_previous_model(self, sess):
 		load = False
 		model_ckpt = '../model/model.meta'
 		if os.path.isfile(model_ckpt):
@@ -29,7 +29,7 @@ class SiameseModel:
 		# start training
 		if load: saver.restore(sess, './model')
 
-	def load_embedding(embed_path):
+	def load_embedding(self, embed_path):
 
 		if not embed_path:
 			exit("ERROR: path to embedding file is invalid!")
@@ -39,7 +39,7 @@ class SiameseModel:
 		self.word_id = {w:i for (i, w) in enumerate(words)}
 		self.id_word = dict(enumerate(words))
 
-	def network_setup():
+	def network_setup(self):
 		# setup siamese network
 		self.siamese_net = siamese();
 		self.train_step = tf.train.AdamOptimizer().minimize(siamese.loss)
@@ -47,7 +47,7 @@ class SiameseModel:
 		tf.initialize_all_variables().run()
 
 
-	def train_on_batch(data_x1, data_x2, data_y, cur_iter):
+	def train_on_batch(self, data_x1, data_x2, data_y, cur_iter):
 		_, loss = sess.run([self.train_step, self.siamese_net.loss], feed_dict={
                         siamese.x1: data_x1,
                         siamese.x2: data_x2,
@@ -56,23 +56,30 @@ class SiameseModel:
 			print ('step %d: loss %.3f' % (step, loss))
 
 
-	def run():
-		# first load in pretrained embedding
-		embeddings, word_id, id_word  = load_embedding("../embed/polyglot-zh.pkl")
+	def run(self):
 
+		print("######## Now load embeddings ########")
+
+		# first load in pretrained embedding
+		embeddings, word_id, id_word  = self.load_embedding("../embed/polyglot-zh.pkl")
+
+		print("######## Now load data ########")
 		# then load in provided data, and split into train and test set
 		data_list = read_csv("../data/atec_nlp_sim_train.csv")
 		train_data, test_data = prepare_train_test(data_list)
 
+		train_data_x1, train_data_x2, train_data_y = load_batches(train_data, batch_size=32) # load training data into batches
+		self.test_data_x1, self.test_data_x2, self.test_data_y = load_batches(test_data, batch_size=len(test_data)) # transform testing data into easy form 
+
+		print("######## Model initialization ########")
 		# initialize the model
 		siamese_net, train_step, saver = network_setup()
 
 		sess = tf.session() # open a new session
 		load_previous_model(sess) # ask if want to load a pre-trained model
 
-		train_data_x1, train_data_x2, train_data_y = load_batches(train_data, batch_size=32) # load training data into batches
-		self.test_data_x1, self.test_data_x2, self.test_data_y = load_batches(test_data, batch_size=len(test_data)) # transform testing data into easy form 
 
+		print("######## Here we go! ########")
 		num_batches = len(train_data_x1) 
 		cur_iter = 0
 		for epoch in xrange(self.num_epochs):
@@ -84,9 +91,9 @@ class SiameseModel:
 				if cur_iter % self.test_gap == 0:
 					evaluate()
 				if cur_iter % self.save_gap == 0:
-					saver.save(../model/model)
+					self.saver.save("../model/model")
 
-	def evaluate():
+	def evaluate(self):
 		# first get the cosine similarity of each pair in testing set
 		cosine_sim = sess.run([self.siamese_net.cosine_sim], feed_dict={
                         siamese.x1: self.test_data_x1,
@@ -100,6 +107,7 @@ class SiameseModel:
 
 		accuracy = np.sum(judge_res == self.test_data_y) * 1.0 / len(self.test_data_y)
 		print("\nAccuracy on validation set: " + str(accuracy) + "\n")
+
 
 if __name__ == "__main__":
 
